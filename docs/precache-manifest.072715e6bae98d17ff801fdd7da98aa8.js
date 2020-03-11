157890c0f51183cc774e8afb6143fefa7ee5ee96
Alexander Letnikow <reclamme@gmail.com>
2020-03-11 17:17:50 +0200

    Add GH Pages build

---
+++ docs/precache-manifest.072715e6bae98d17ff801fdd7da98aa8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036c4464c6800b5cc4c373abb17d1602",
+    "url": "./index.html"
+  },
+  {
+    "revision": "83fa7d296c079fbcf33d",
+    "url": "./static/css/main.5f6a6424.chunk.css"
+  },
+  {
+    "revision": "94b722fc2bc74428f8c8",
+    "url": "./static/js/2.ffd92a94.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.ffd92a94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83fa7d296c079fbcf33d",
+    "url": "./static/js/main.36acd126.chunk.js"
+  },
+  {
+    "revision": "222fe1d50ce2a5776fd9",
+    "url": "./static/js/runtime-main.eeb3c59c.js"
+  },
+  {
+    "revision": "7b6df12652d63b336a3389c7c4743bce",
+    "url": "./static/media/icon-arrow.7b6df126.svg"
+  },
+  {
+    "revision": "404339d082774904446406878a7b2054",
+    "url": "./static/media/icon-buy.404339d0.svg"
+  },
+  {
+    "revision": "9463f2b5e5efc42898578b907e4e1ec6",
+    "url": "./static/media/icon-sale.9463f2b5.svg"
+  }
+]);
